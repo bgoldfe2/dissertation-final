1e598de6ef0e88bbe6213a5baf3381a85f9e46dc
bruce-goldfeder-asetpartners <bruce.goldfeder@asetpartners.com>
2023-04-24 10:29:17 -0400

    Begin testing the five class data set

--- Scripts/driver.py
+++ Scripts/driver.py
@@ -36,7 +36,7 @@ def get_parser():
     parser.add_argument("--train_batch_size", default=16, type=int,  help='Training batch size')
     parser.add_argument("--valid_batch_size", default=32, type=int,  help='Validation batch size')
     parser.add_argument("--test_batch_size", default=32, type=int,  help='Test batch size')
-    parser.add_argument("--epochs", default=4, type=int,  help='Number of training epochs')
+    parser.add_argument("--epochs", default=1, type=int,  help='Number of training epochs')
     parser.add_argument("-lr","--learning_rate", default=2e-5, type=float,  help='The learning rate to use')
     parser.add_argument("-wd","--weight_decay", default=1e-4, type=float,  help=' Decoupled weight decay to apply')
     parser.add_argument("--adamw_epsilon", default=1e-8, type=float,  help='AdamW epsilon for numerical stability')
@@ -89,6 +89,8 @@ if __name__=="__main__":
     args.model_list = model_list
     my_args = utils.create_folders(args)
 
+    # Test the Five Class run
+    args.classes = 5
 
     print("args type in driver main after create_folders ", type(args))
     train_all_models(args)
